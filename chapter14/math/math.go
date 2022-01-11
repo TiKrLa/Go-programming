08a18268b73cb07e1f2ccf3914ff0d1c9b436eed
Tia Lapinjoki <tia.lapinjoki14@gmail.com>
2022-01-11 16:03:47 +0200

    Add files via upload

---
+++ chapter14/math/math.go
@@ -0,0 +1,26 @@
+package math
+
+func Min(xs []float64) float64 {
+	if len(xs) == 0 {
+		return 0
+	}
+	m := xs[0]
+	for _, x := range xs[1:] {
+		if x < m {
+			m = x
+		}
+	}
+	return m
+}
+func Max(xs []float64) float64 {
+	if len(xs) == 0 {
+		return 0
+	}
+	m := xs[0]
+	for _, x := range xs[1:] {
+		if x > m {
+			m = x
+		}
+	}
+	return m
+}
